a6817de46edba912dcd3f5c4c36db3da2d85642c
Ivan_G0nzalez <iegdlt@gmail.com>
2023-10-02 15:46:45 -0500

    vite config was changed

--- src/pages/NoFound/index.tsx
+++ src/pages/NoFound/index.tsx
@@ -2,8 +2,7 @@ function NotFoundPage() {
   return (
     <div className="flex justify-center">
       <img
-        // eslint-disable-next-line no-octal-escape
-        src="src\assets\images\NoFound\empty.gif"
+        src="src/assets/images/NoFound/empty.gif"
         alt="emty"
         className="max-w-xs w-full"
       />
