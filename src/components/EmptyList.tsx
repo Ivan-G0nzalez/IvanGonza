3791b02d551797d9bf7025ea2b5dc7e663ce20e8
Ivan_G0nzalez <iegdlt@gmail.com>
2023-10-02 16:19:42 -0500

    Deployment almost done

---
+++ src/components/EmptyList.tsx
@@ -0,0 +1,13 @@
+function EmtyList() {
+  return (
+    <div className="flex justify-center">
+      <img
+        src="../src/assets/images/NoFound/empty.gif"
+        alt="emty"
+        className="max-w-xs w-full"
+      />
+    </div>
+  );
+}
+
+export default EmtyList;
